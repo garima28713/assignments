fe1efeb8752b370472e3ed8799a3d9a510f7cacd
garima28713 <garima28713@gmail.com>
2023-06-07 14:37:13 +0530

    assignment 1 and 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/employee.interface.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+    employeeID:number,
+    firstName:string,
+    lastName:string,
+    salary:string,
+    dob:string,
+    email:string,
+    action?:boolean
+
+}
